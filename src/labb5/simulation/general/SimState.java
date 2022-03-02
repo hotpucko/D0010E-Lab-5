6fa93a1a4349bb2fcc5b2c53ad565abfd3361a07
Hotpucko <stn.jonsson@gmail.com>
2022-03-02 17:41:59 +0100

    Fixed formatting

--- src/labb5/simulation/general/SimState.java
+++ src/labb5/simulation/general/SimState.java
@@ -3,42 +3,43 @@ package labb5.simulation.general;
 import java.util.Observable;
 
 public abstract class SimState extends Observable {
-	
+
 	protected double lastEventTime;
 	protected boolean isRunning;
 	private double timeElapsed;
 	private boolean emergencyBreak;
-	
+
 	public SimState() {
-		
+
 	}
-	
+
 	void update(Event e) {
 		if (!isRunning()) {
 			return;
 		}
-		
-		if (!emergencyBreak)
+
+		if (!emergencyBreak) {
 			this.timeElapsed += e.getTime() - lastEventTime;
-		
+		}
+
 		setChanged();
 		notifyObservers(e);
 		lastEventTime = e.getTime();
 	}
-	
+
 	public boolean isRunning() {
 		return isRunning();
 	}
-	
+
 	void EmergencyBreak() {
 		this.emergencyBreak = true;
 		this.isRunning = false;
 	}
-	
+
 	double getCurrentTime() {
 		return this.timeElapsed;
 	}
-	
+
 	protected boolean isEmergencyBreak() {
 		return emergencyBreak;
 	}
