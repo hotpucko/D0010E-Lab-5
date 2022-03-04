f0e840d9871adc1474c720fd8b2754b436a30b0e
Hotpucko <stn.jonsson@gmail.com>
2022-03-04 17:39:52 +0100

    SupermarketState.java: Declared variables

--- src/labb5/simulation/supermarket/state/SupermarketState.java
+++ src/labb5/simulation/supermarket/state/SupermarketState.java
@@ -4,6 +4,23 @@ import labb5.simulation.general.SimState;
 
 public class SupermarketState extends SimState {
 
+	private int seed;
+	private int currentRegister;
+	private int maxRegisters;
+	private int customerMax;
+	private int customersInShop;
+	private int customersRejected;
+	private int customersProcessed;
+	private int totalCustomersQueued;
+	private boolean isShopOpen;
+	private double totalTimeQueued;
+	private double totalTimeRegistersIdled;
+	private double lambda;
+	private double kMin;
+	private double kMax;
+	private double pMin;
+	private double pMax;
+	
 	public SupermarketState() {
 		// TODO Auto-generated constructor stub
 	}
