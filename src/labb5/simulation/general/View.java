e6ce91334c43cd285015862cc1caf2185548f437
Hotpucko <stn.jonsson@gmail.com>
2022-03-07 14:41:25 +0100

    Added javadocs

--- src/labb5/simulation/general/View.java
+++ src/labb5/simulation/general/View.java
@@ -2,8 +2,22 @@ package labb5.simulation.general;
 
 import java.util.Observer;
 
-public abstract class View implements Observer{
+/**
+ * <code>View</code> is the abstract implementation for handling all contexts
+ * related to analysis of the SimState object
+ * 
+ * @author Stefan Jonsson, Dino Lolic, William Kiwanuka, Arvid From
+ * @version %I%, %G%
+ *
+ */
+public abstract class View implements Observer {
 
+	/**
+	 * The view constructor handles setting up the Observer pattern
+	 * 
+	 * @param state
+	 *            The state object for the view to observe
+	 */
 	public View(SimState state) {
 		state.addObserver(this);
 	}
