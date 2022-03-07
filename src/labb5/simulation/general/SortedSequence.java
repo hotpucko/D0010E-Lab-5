b5ebe9b3ddee721ba3d0854fd2715570ec52d598
arvid <arvidfrom93@gmail.com>
2022-03-07 17:42:59 +0100

    corrected JavaDocs

--- src/labb5/simulation/general/SortedSequence.java
+++ src/labb5/simulation/general/SortedSequence.java
@@ -2,7 +2,7 @@ package labb5.simulation.general;
 import java.util.Vector;
 import java.util.NoSuchElementException;
 
-/*
+/**
  * @author Dino Lolic, William Kiwanuka, Stefan Jonsson, Arvid From
  */
 public class SortedSequence<E extends Comparable<E>> 
@@ -10,14 +10,14 @@ implements Comparable<SortedSequence<E>> {
 	
 	private Vector<E> store = new Vector<E>();
 	
-	/*
+	/**
 	 * adds item to the sorted sequence
 	 */
 	public void add(E item) {
 		_add (item);
 	}
 	
-	/*
+	/**
 	 * private implementation of add(E);
 	 *throws an exception if there is no item to put in the sequence
 	 *sorts the item based on the size of a comparable variable
@@ -39,20 +39,20 @@ implements Comparable<SortedSequence<E>> {
 				}
 		}
 	}
-	/*
+	/**
 	 * returns the first element in the sequence
 	 *  @return the first element in the sequence
 	 */
 	public E smallest() {
 		return _smallest();
 	}
-	/*
+	/**
 	 * private implementation of smallest()
 	 */
 	private E _smallest() {
 		return store.elementAt(0);
 	}
-	/*
+	/**
 	 * tests if the sequence has no components
 	 * @return true if the sequence has no components
 	 */
@@ -60,7 +60,7 @@ implements Comparable<SortedSequence<E>> {
 		return _isEmpty();
 	}
 	
-	/*
+	/**
 	 * tests if the sequence has no components
 	 * @return true if the sequence has no components
 	 */
@@ -69,14 +69,14 @@ implements Comparable<SortedSequence<E>> {
 	return store.isEmpty();
 	}
 	
-	/*
+	/**
 	 * removes the smallest comparable object of the sequence
 	 */
 	public void removeSmallest() throws NoSuchElementException{
 		_removeSmallest();
 	}
 	
-	/*
+	/**
 	 * removes the smallest comparable object of the sequence,
 	 * throws exception if no objects exist in the sequence
 	 */
@@ -87,21 +87,21 @@ implements Comparable<SortedSequence<E>> {
 			throw new NoSuchElementException();
 		}
 	}
-	/*
+	/**
 	 * returns the size of the sorted sequence
 	 * @return  the size of the sorted sequence
 	 */
 	public int size() {
 		return _size();	
 }
-	/*
+	/**
 	 * returns the size of the sorted sequence
 	 * @return  the size of the sorted sequence
 	 */
 	private int _size() {
 		return store.size();
 	}
-	/*
+	/**
 	 * compares ordered sequences
 	 * 
 	 * @return  -1, 0,or 1 depending on whether
@@ -110,7 +110,7 @@ implements Comparable<SortedSequence<E>> {
 		public int compareTo(SortedSequence<E> o) {
 			return _compareTo(o);
 	}
-		/*
+		/**
 		 * compares ordered sequences
 		 * 
 		 * @return  -1, 0,or 1 depending on whether
