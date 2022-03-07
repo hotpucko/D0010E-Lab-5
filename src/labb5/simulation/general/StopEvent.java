f065e104a6eb4594f33dd8b54d8ae474acfb344b
Slaktish1 <93648378+Slaktish1@users.noreply.github.com>
2022-03-07 13:33:00 +0100

    Fixed constructor and class javadoc

--- src/labb5/simulation/general/StopEvent.java
+++ src/labb5/simulation/general/StopEvent.java
@@ -4,16 +4,19 @@ import labb5.simulation.general.EventQueue;
 import labb5.simulation.general.SimState;
 import labb5.simulation.supermarket.state.SupermarketState;
 
+/**
+ * This class represents the event of the simulation being fully completed.
+ * 
+ * @author Dino Lolic, William Kiwanuka, Stefan Jonsson, Arvid From
+ */
 public class StopEvent extends Event {
 
 	/**
-	 * This class represents the event of the simulation being fully completed.
+	 * The constructor for the simulation stop Note that time is set to 999.
 	 * 
-	 * @param simState
-	 * @param eventQueue
-	 * @param time
-	 * 
-	 * @author Dino Lolic, William Kiwanuka, Stefan Jonsson, Arvid From
+	 * @param simState   The simulation to stop
+	 * @param eventQueue Needed for general Event class
+	 * @param time       The current absolute time, set to 999.
 	 */
 	public StopEvent(SimState simState, EventQueue eventQueue, double time) {
 		super(simState, eventQueue, 999);
@@ -26,7 +29,7 @@ public class StopEvent extends Event {
 	public String toString() {
 		return "Stop";
 	}
-	
+
 	/**
 	 * Runs the stopEvent, which essentially stops the simulation.
 	 */
