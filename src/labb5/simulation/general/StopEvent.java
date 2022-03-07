3ac72f5b049dcc6833ab9e6669a11ff1487a499e
Slaktish1 <93648378+Slaktish1@users.noreply.github.com>
2022-03-07 10:00:03 +0100

    Revert "Stop event"

--- src/labb5/simulation/general/StopEvent.java
+++ src/labb5/simulation/general/StopEvent.java
@@ -1,33 +1,12 @@
 package labb5.simulation.general;
 
-import labb5.simulation.general.EventQueue;
-import labb5.simulation.general.SimState;
-import labb5.simulation.supermarket.state.SupermarketState;
+import general.EventQueue;
+import general.SimState;
 
 public class StopEvent extends Event {
 
-	/**
-	 * This class represents the event of the simulation being fully completed.
-	 * 
-	 * @param simState
-	 * @param eventQueue
-	 * @param time
-	 * 
-	 * @author Dino Lolic, William Kiwanuka, Stefan Jonsson, Arvid From
-	 */
-	public StopEvent(SimState simState, EventQueue eventQueue, double time) {
-		super(simState, eventQueue, 999);
+	public 	StopEvent(SimState simState, EventQueue eventQueue, double time) {
+		super(simState, eventQueue, time);
 	}
 
-	@Override
-	public String toString() {
-		return "Stop";
-	}
-
-	@Override
-	public void run(SimState simState) {
-		SupermarketState state = (SupermarketState) this.simState;
-		state.emergencyBreak(); // Signal that the simulation is done.
-		state.update();
-	}
 }
