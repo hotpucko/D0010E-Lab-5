c08235791c664d007a617599a122995c2b2a6516
arvid <arvidfrom93@gmail.com>
2022-03-08 13:22:03 +0100

    Implemented class
    Wrote specific implementation  of the general StopEvent.
    Implements a run method which ends the simulation and a toString method
    for later use.

--- src/labb5/simulation/supermarket/events/StopShopEvent.java
+++ src/labb5/simulation/supermarket/events/StopShopEvent.java
@@ -1,11 +1,43 @@
 package labb5.simulation.supermarket.events;
 
+import labb5.simulation.general.EventQueue;
+import labb5.simulation.general.SimState;
 import labb5.simulation.general.StopEvent;
 
+/**
+ * Specific implementation of the final event to be put in the eventqueue which
+ * puts an end to the simulation.
+ * 
+ * @author Arvid From, Stefan Jonsson, Dino Lolic, William Kiwanuka
+ * 
+ */
 public class StopShopEvent extends StopEvent {
 
-	public StopShopEvent() {
-		// TODO Auto-generated constructor stub
+	private StopEvent myStopEvent;
+
+	/**
+	 * Constructor which inherits the general parameters of the simulation
+	 */
+	public StopShopEvent(SimState simState, EventQueue eventQueue, double time, int customerNum) {
+		super(simState, eventQueue, time);
+		StopEvent myStopEvent = new StopEvent(simState, eventQueue, time);
+
+	}
+
+	/**
+	 * Stops the simulation
+	 */
+	public void run() {
+		this.myStopEvent.run(simState);
+	}
+
+	/**
+	 * toString method that produces relevant data for representing the sim
+	 * 
+	 * @return string representation of finished sim
+	 */
+	public String toString() {
+		return this.myStopEvent.toString();
 	}
 
 }
