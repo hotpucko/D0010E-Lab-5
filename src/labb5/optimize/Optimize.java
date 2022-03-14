2e663e6642a5bff5d9def45fd82449d2e246916b
arvid <arvidfrom93@gmail.com>
2022-03-14 15:33:26 +0100

    Wrote JavaDocs for Optimize, Runsim and SuperMarketview

--- src/labb5/optimize/Optimize.java
+++ src/labb5/optimize/Optimize.java
@@ -6,69 +6,140 @@ import labb5.simulation.general.RunSim;
 import labb5.simulation.general.SimState;
 import labb5.simulation.supermarket.state.SupermarketState;
 
+/**
+ * Class that implements runSim. The class Minimizes the output of missed
+ * customers as a function of open registers in a given supermarket. The class
+ * consists of three functions, one which just runs a given simulation, one that
+ * controls for least amount of customers missed, and one which massively
+ * implements that simulation to find the correct register given that most other
+ * variables change. The execution is layered, which is to say that: The third
+ * method implements the second, which implements the first.
+ *
+ * 
+ * @author Stefan Jonsson, Arvid From, William Kiwanuka, Dino Lolic
+ *
+ * 
+ */
 public class Optimize {
 
 	Random rnd;
-	
+
 	public Optimize() {
-		
+
 	}
-	
-	//detta �r mitt f�rs�k med optimize metoden, se om ni l�ser det p� n�got b�ttre s�tt :shrug:
-	
-	public SupermarketState metod1(double lambda, double kMin, double kMax, double pMin, double pMax, int seed, int registers, int customerMax, double closingTime) {
+
+	/**
+	 * 
+	 * 
+	 * @param lambda      1/lambda is the rate of customers entering the supermarket
+	 *                    with 1 representing a single unit of time
+	 * @param kMin        The minimum payment time for a single customer
+	 * @param kMax        The maximum payment time for a single customer
+	 * @param pMin        The minimum shopping time for a single customer
+	 * @param pMax        The maximum shopping time for a single customer
+	 * @param seed        The seed that always executes the simulation in exactly
+	 *                    the same way given a randomiser
+	 * @param registers   The amount of Registers available throughout the
+	 *                    simulation
+	 * @param customerMax The maximum amount of customers allowed within the
+	 *                    supermarket
+	 * @param closingTime After how many units of time does the shop being simulated
+	 *                    close
+	 * 
+	 * @return returns the Supermarket state after a single regular simulation of a
+	 *         supermarket with given parameters
+	 */
+
+	public SupermarketState metod1(double lambda, double kMin, double kMax, double pMin, double pMax, int seed,
+			int registers, int customerMax, double closingTime) {
 		RunSim sim = new RunSim(customerMax, lambda, kMin, kMax, pMin, pMax, seed, registers);
 		sim.run(closingTime);
 		return sim.getState();
-		//return simState; //sim.state beh�ver visst vara public, sorry arvid, eller g�r en getter f�r simstate :shrug:
 	}
-	
-	public SupermarketState metod2(double lambda, double kMin, double kMax, double pMin, double pMax, int seed, int customerMax, double closingTime, int min, int max) {
-		
-		//base case
+
+	/**
+	 * Locates the least amount of registers needed for a supermarket to miss the
+	 * least amount of customers, all other things equal. Uses repeated iterations
+	 * of metod1() and a recursive binary search to achieve this
+	 * 
+	 * @param lambda      1/lambda is the rate of customers entering the supermarket
+	 *                    with 1 representing a single unit of time
+	 * @param kMin        The minimum payment time for a single customer
+	 * @param kMax        The maximum payment time for a single customer
+	 * @param pMin        The minimum shopping time for a single customer
+	 * @param pMax        The maximum shopping time for a single customer
+	 * @param seed        The seed that always executes the simulation in exactly
+	 *                    the same way given a randomiser
+	 * @param customerMax The max amount of customers allowed within a single shop
+	 *                    within the customerSimulator
+	 * @param closingTime After how many units of time does the shop being simulated
+	 *                    close
+	 * @param min         Low bound for binary search of the results for all the
+	 *                    simulations during runtime
+	 * @param max         High bound for binary search of the results for all the
+	 *                    simulations during runtime
+	 * @return A Supermarket-state which produces the least amount of missed
+	 *         customers as a function of the amount of open registers
+	 */
+	public SupermarketState metod2(double lambda, double kMin, double kMax, double pMin, double pMax, int seed,
+			int customerMax, double closingTime, int min, int max) {
+
+		// base case
 		if (min == max) {
 			return metod1(lambda, kMin, kMax, pMin, pMax, seed, min, customerMax, closingTime);
 		}
-		
-		SupermarketState middle = metod1(lambda, kMin, kMax, pMin, pMax, seed, Math.floorDiv(min + max, 2), customerMax, closingTime);
+
+		SupermarketState middle = metod1(lambda, kMin, kMax, pMin, pMax, seed, Math.floorDiv(min + max, 2), customerMax,
+				closingTime);
 		SupermarketState upper = metod1(lambda, kMin, kMax, pMin, pMax, seed, max, customerMax, closingTime);
-		
-		//recursive case
-		System.out.println(String.format("Middle: %d | Upper: %d | min: %d | max: %d", middle.getCustomersRejected(), upper.getCustomersRejected(), min, max));
-		if(middle.getCustomersRejected() <= upper.getCustomersRejected())
-			return metod2(lambda, kMin, kMax, pMin, pMax, seed, customerMax, closingTime, min, Math.floorDiv(min + max, 2));
-		return metod2(lambda, kMin, kMax, pMin, pMax, seed, customerMax, closingTime, (int)Math.ceil(new Double(min + max)/2d), max);
-		/*
-		if (lowerRecursion.getCustomersRejected() <= upperRecursion.getCustomersRejected()) {
-			return lowerRecursion;
-		}
-		return upperRecursion;
-		*/
-		
-		
-		/*int registers = customerMax;
-		
-		int missedCustomers = metod1(lambda, kMin, kMax, pMin, pMax, seed, registers, customerMax, closingTime);
 
-		while (registers > 0) {
-			int newMissedCustommers = metod1(lambda, kMin, kMax, pMin, pMax, seed, registers, customerMax, closingTime);
-
-			if (newMissedCustommers != missedCustomers)
-				return registers + 1;
-			registers--;
-		}
-		return customerMax;*/
+		// recursive case
+		System.out.println(String.format("Middle: %d | Upper: %d | min: %d | max: %d", middle.getCustomersRejected(),
+				upper.getCustomersRejected(), min, max));
+		if (middle.getCustomersRejected() <= upper.getCustomersRejected())
+			return metod2(lambda, kMin, kMax, pMin, pMax, seed, customerMax, closingTime, min,
+					Math.floorDiv(min + max, 2));
+		return metod2(lambda, kMin, kMax, pMin, pMax, seed, customerMax, closingTime,
+				(int) Math.ceil(new Double(min + max) / 2d), max);
 	}
-	
-	public SupermarketState metod3(double lambda, double kMin, double kMax, double pMin, double pMax, int seed, int customerMax, double closingTime) {
+
+	/**
+	 * Locates the least amount of registers needed to to reduce the amount of
+	 * missed customers to its smallest possible value in a given supermarket state
+	 * Assures that said data is dependable by imposing that the return value must
+	 * be the product of a 100 randomly varying simulations in a row before ending
+	 * up as the return value
+	 * 
+	 * @param lambda      1/lambda is the rate of customers entering the supermarket
+	 *                    with 1 representing a single unit of time
+	 * @param kMin        The minimum payment time for a single customer
+	 * @param kMax        The maximum payment time for a single customer
+	 * @param pMin        The minimum shopping time for a single customer
+	 * @param pMax        The maximum shopping time for a single customer
+	 * @param seed        The seed that always executes the simulation in exactly
+	 *                    the same way given a randomiser
+	 * @param customerMax The max amount of customers allowed within a single shop
+	 *                    within the customerSimulator
+	 * @param closingTime After how many units of time does the shop being simulated
+	 *                    close
+	 * @return for a given seed of random values, the method returns the smallest
+	 *         amount of registers needed to reduce the amount of missed customers
+	 *         to its smallest possible value.
+	 */
+
+	public SupermarketState metod3(double lambda, double kMin, double kMax, double pMin, double pMax, int seed,
+			int customerMax, double closingTime) {
 		rnd = new Random(seed);
 		int iterationsSinceLastChange = 0;
 		SupermarketState worstRegistersAmount = null;
 
 		while (iterationsSinceLastChange <= 100) {
-			SupermarketState currentRegistersAmount = metod2(lambda, kMin, kMax, pMin, pMax, rnd.nextInt(), customerMax, closingTime, 1, customerMax);
-			System.out.println(String.format("Method2 returned %d registers with %d customers rejected", currentRegistersAmount.getMaxRegistersCount(), currentRegistersAmount.getCustomersRejected()));
-			if (worstRegistersAmount == null || currentRegistersAmount.getMaxRegistersCount() > worstRegistersAmount.getMaxRegistersCount()) {
+			SupermarketState currentRegistersAmount = metod2(lambda, kMin, kMax, pMin, pMax, rnd.nextInt(), customerMax,
+					closingTime, 1, customerMax);
+			System.out.println(String.format("Method2 returned %d registers with %d customers rejected",
+					currentRegistersAmount.getMaxRegistersCount(), currentRegistersAmount.getCustomersRejected()));
+			if (worstRegistersAmount == null
+					|| currentRegistersAmount.getMaxRegistersCount() > worstRegistersAmount.getMaxRegistersCount()) {
 				worstRegistersAmount = currentRegistersAmount;
 				iterationsSinceLastChange = 0;
 				continue;
--- src/labb5/simulation/supermarket/view/SupermarketView.java
+++ src/labb5/simulation/supermarket/view/SupermarketView.java
@@ -11,13 +11,24 @@ import labb5.simulation.supermarket.events.ClosingEvent;
 import labb5.simulation.supermarket.events.PayLeaveEvent;
 import labb5.simulation.supermarket.events.ShoppingEvent;
 import labb5.simulation.supermarket.state.SupermarketState;
-
+/**
+ * Class which implements a view and a console output for the specific supermarket simulation
+ * 
+ * @author Arvid From, Stefan Jonsson, Dino Lolic, William Kiwanuka
+ *
+ */
 public class SupermarketView extends View{
-
+/**
+ * Constructor of the view, makes it possible to observe the state
+ * @param The state which is to be observed
+ */
 	public SupermarketView(SupermarketState state) {
 		super(state);
 	}
 
+/**
+ * Updates the view to reflect the current status of the supermarket
+ */
 	@Override
 	public void update(Observable stateObject, Object event) {
 		//SimState state = (SimState)stateObject;
@@ -25,7 +36,12 @@ public class SupermarketView extends View{
 		Event e = (Event)event;
 		printEvent(state, e);
 	}
-
+/**
+ * Compilation of prints needed for the view
+ * 
+ * @param state which is observed
+ * @param event which occurs to trigger the changing of the state
+ */
 	private void printEvent(SupermarketState state, Event e) {
 		if(e instanceof StartEvent) {
 			
@@ -38,12 +54,16 @@ public class SupermarketView extends View{
 			printCustomerEvents(state, e);
 		}
 	}
-
+/**
+ * Console representation  of each  simulation
+ * @param state state which was observed
+ * @param Event which occurs to trigger a change in the state
+ */
 	private void printCustomerEvents(SupermarketState state, Event e) {
 		String time = String.format("%.2f", state.getCurrentTime());//String.valueOf(format(state.getCurrentTime()));
 		String eventName = e.toString();
 		String customerNumber = eventToCustomerNumberString(e);
-		String open = state.isOpen() ? "ö" : "s";
+		String open = state.isOpen() ? "Ã¶" : "s";
 		String registers = String.valueOf(state.getFreeRegisters());
 		String timeRegistersIdle = String.format("%.2f", state.getTimeRegistersIdled());
 		String customersInStore = String.valueOf(state.getCustomersInShop());
@@ -61,21 +81,33 @@ public class SupermarketView extends View{
 				registerQueue
 				));
 	}
-
+/**
+ * Prints the closing event
+ * @param e the closing event
+ */
 	private void printClosingEvent(Event e) {
 		System.out.println(String.format("%.2f\t%-8s\t%s", e.getTime(), e.toString(), this.eventToCustomerNumberString(e)));
 	}
 
+	/**
+	 * Prints the tail of the simulation
+	 * @param state the state which is meant to be printed
+	 * @param e	The stopEvent which ceases the simulation
+	 */
 	private void printStopEvent(SupermarketState state, Event e) {
 		System.out.println(String.format("%.2f\t%s", e.getTime(), e.toString()));
 		System.out.println("RESULTAT\n========\n");
 		System.out.println(String.format("1)\tav %d kunder handlade %d medan %d missades\n", state.getCustomersProcessed() + state.getCustomersRejected(), state.getCustomersProcessed(), state.getCustomersRejected()));
 		System.out.println(String.format("2)\tTotal tid %d kassor varit lediga: %.2f te.", state.getMaxRegistersCount(), state.getTimeRegistersIdled()));
-		System.out.println(String.format("\tGenomsnittlig ledig kassatid: %.2f te (dvs %.2f%% av tiden från öppning tills sista kunden betalat)\n", state.getTimeRegistersIdled() / state.getMaxRegistersCount(), ((state.getTimeRegistersIdled() / state.getMaxRegistersCount()) / state.getCurrentTime() * 100)));
-		System.out.println(String.format("3)\tTotal tid %d kunder tvingats köa: %.2f te.", state.getTotalCustomersQueued(), state.getTotalTimeQueued()));
-		System.out.println(String.format("\tGenomsnittlig kötid: %.2f te.", state.getTotalTimeQueued() / state.getTotalCustomersQueued()));
+		System.out.println(String.format("\tGenomsnittlig ledig kassatid: %.2f te (dvs %.2f%% av tiden frÃ¥n Ã¶ppning tills sista kunden betalat)\n", state.getTimeRegistersIdled() / state.getMaxRegistersCount(), ((state.getTimeRegistersIdled() / state.getMaxRegistersCount()) / state.getCurrentTime() * 100)));
+		System.out.println(String.format("3)\tTotal tid %d kunder tvingats kï¿½a: %.2f te.", state.getTotalCustomersQueued(), state.getTotalTimeQueued()));
+		System.out.println(String.format("\tGenomsnittlig kÃ¶tid: %.2f te.", state.getTotalTimeQueued() / state.getTotalCustomersQueued()));
 	}
-
+/**
+ * prints the head of  the superMarketview
+ * @param state The state which is to be observed
+ * @param e the StartEvent  that begins the simulation
+ */
 	private void printStartEvent(SupermarketState state, Event e) {
 		System.out.println("PARAMETRAR");
 		System.out.println("==========");
@@ -84,13 +116,17 @@ public class SupermarketView extends View{
 		System.out.println(String.format("Ankomsthastighet, lambda.: %f", state.getLambda()));
 		System.out.println(String.format("Plocktider, [P_min..Pmax]: [%f..%f]", state.getPMin(), state.getPMax()));
 		System.out.println(String.format("Betaltider, [K_min..Kmax]: [%s..%s]", state.getKMin(), state.getKMax()));
-		System.out.println(String.format("Frö, f...................: %d", state.getSeed()));
-		System.out.println("\nFÖRLOPP");
+		System.out.println(String.format("FrÃ¶, f...................: %d", state.getSeed()));
+		System.out.println("\nFÃ–RLOPP");
 		System.out.println("=======");
-		System.out.println("Tid\tHändelse\tKund\t?\tled\tledT\tI\t$\t:-(\tköat\tköT\tköar\t[KassaKö]");
+		System.out.println("Tid\tHÃ¤ndelse\tKund\t?\tled\tledT\tI\t$\t:-(\tkÃ¶at\tkÃ¶T\tkÃ¶ar\t[KassaKÃ¶]");
 		System.out.println(String.format("%.2f\t%s", e.getTime(), e.toString()));
 	}
-	
+	/**
+	 * Gets customerNumbers for each event
+	 * @param e The event from which the customerNumber is meant to be retrieved
+	 * @return	returns a string with the events customerNumber
+	 */
 	private String eventToCustomerNumberString(Event e)
 	{
 		if(e instanceof StartEvent || e instanceof StopEvent) {
